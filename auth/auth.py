d2b4e4197c02d70a443a32dfc6a80e5f8bdea4c6
Ranko Colevic <rankocolevic@gmail.com>
2022-05-24 12:07:36 +0200

    Bycript updat

--- auth/auth.py
+++ auth/auth.py
@@ -2,8 +2,10 @@ from flask import Blueprint, jsonify, request
 from db import db 
 from model import User
 from validate_email_address import validate_email
-bp = Blueprint('auth',__name__,url_prefix="/usr")
+from flask_bcrypt import Bcrypt
 
+bc = Bcrypt() 
+bp = Blueprint('auth',__name__,url_prefix="/usr")
 # For update: Add bycrypt to make a hash password in register and into login to check hash passowrd
 
 @bp.route('/register', methods=(["POST","GET"]))
@@ -12,12 +14,13 @@ def register():
     email = request.json.get('email')
     password = request.json.get('password')
     conf_password = request.json.get('conf_password')
+    hash_pw = bc.generate_password_hash(password)
     email_check = validate_email(email, verify=True)
     if password != conf_password:
         return jsonify(detail="Error! password need to be same"), 409
 
     if email_check == True:
-        insert_query = User(username=username, email=email, password=password)
+        insert_query = User(username=username, email=email, password=hash_pw)
         db.session.add(insert_query)
         db.session.commit()
         return jsonify(detail="Register Sec welcome"), 200
@@ -29,10 +32,11 @@ def register():
 def login():
     username = request.json.get('username')
     password = request.json.get('password')
-
-    check_user = User.query.filter(User.username == username and User.password == password).first()
-
-    if check_user:
+    check_user = User.query.filter(User.username == username).first()
+    if check_user == None:
+        return jsonify(detail="Error! User not found"), 404
+    check_password = bc.check_password_hash(check_user.password, password)
+    if check_user and check_password:
         return jsonify(detail="Welcome"), 200
     else:
         return jsonify(detail="Error! User not found"), 404
